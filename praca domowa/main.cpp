b9a56e52d78f58e8327b4ff9659389621484f13a
Dosiekkk <97113008+Dosiekkk@users.noreply.github.com>
2022-01-04 16:54:52 +0100

    Add files via upload

---
+++ praca domowa/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+string a;
+int main()
+{
+    cout << "Witaj! Podaj imie:" << endl;
+    cin  >> a;
+    cout << "Szczesliwego nowego roku " << a << "!"<< endl;
+
+    return 0;
+}
